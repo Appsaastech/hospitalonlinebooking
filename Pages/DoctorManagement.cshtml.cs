7d843ee58f55414d2b2e77f580f75c76fbd26251
anupama <anupamamangalathu@gmail.com>
2026-05-11 20:48:43 +0530

    Updated doctor management pages

---
+++ Pages/DoctorManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hospitalonlinebooking.Pages
+{
+    public class DoctorManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
