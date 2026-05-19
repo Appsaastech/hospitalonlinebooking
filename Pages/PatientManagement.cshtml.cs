7e1608753c4b26dda4536851f0efb6fd187bb928
anupama <anupamamangalathu@gmail.com>
2026-05-19 20:23:52 +0530

    updated publc appointment page

---
+++ Pages/PatientManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hospitalonlinebooking.Pages
+{
+    public class Patient_ManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/PrefixManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hospitalonlinebooking.Pages
+{
+    public class PrefixManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
