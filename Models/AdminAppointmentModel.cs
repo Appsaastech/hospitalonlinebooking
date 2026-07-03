3269541dc5c97fdd1735e4438a2e3888553f6d07
anupama <anupamamangalathu@gmail.com>
2026-07-03 12:17:18 +0530

    updated

--- Models/AdminAppointmentModel.cs
+++ Models/AdminAppointmentModel.cs
@@ -2,5 +2,32 @@
 {
     public class AdminAppointmentModel
     {
+        public int Id { get; set; }
+
+        // Patient Details
+        public string PatientRegistrationNo { get; set; } = string.Empty;
+
+        public string PatientName { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        // Appointment Details
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; } = string.Empty;
+
+        public int DoctorId { get; set; }
+
+        public string DoctorName { get; set; } = string.Empty;
+
+        public int ScheduleId { get; set; }
+
+        public string ScheduleDate { get; set; } = string.Empty;
+
+        public string DayName { get; set; } = string.Empty;
+
+        public string AppointmentTime { get; set; } = string.Empty;
+
+        public string TimeSlots { get; set; } = string.Empty;
     }
 }
--- Models/PublicAppointmentModel.cs
+++ Models/PublicAppointmentModel.cs
@@ -6,15 +6,20 @@
 
         public string PatientRegistrationNo { get; set; } = string.Empty;
 
-        public string DepartmentId { get; set; } = string.Empty;
+        public int DepartmentId { get; set; }
 
         public string DepartmentName { get; set; } = string.Empty;
 
-        public string DoctorId { get; set; } = string.Empty;
+        public int DoctorId { get; set; }
 
         public string DoctorName { get; set; } = string.Empty;
 
         public string ScheduleDate { get; set; } = string.Empty;
+        public string Day { get; set; } = string.Empty;
+
+        public string AvailableSlots { get; set; } = string.Empty;
+
+        public int ScheduleId { get; set; }
 
         public string AppointmentTime { get; set; } = string.Empty;
 
